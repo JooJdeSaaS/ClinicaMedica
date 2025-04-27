20796103d952b8720543293d6462e26a45aacbf1
fenrir-mack <10436455@mackenzista.com.br>
2025-04-27 05:34:13 -0300

    somente criação do MeuCadastroDAO.java

---
+++ src/main/java/com/mack/clinica/model/MeuCadastroDAO.java
@@ -0,0 +1,40 @@
+package com.mack.clinica.model;
+
+import com.mack.clinica.util.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MeuCadastroDAO {
+
+    private String realPathBase;
+
+    public MeuCadastroDAO(String realPathBase) {
+        this.realPathBase = realPathBase;
+    }
+
+//TODO fazer a mudança de informações
+    public static void editarUsuario(String email, String senha, String realPathBase) {
+        try (Connection conn = DatabaseConnection.getConnection(realPathBase)) {
+            String sql = "SELECT id, nome, tipo, cpf, celular FROM usuarios WHERE email = ? AND senha = ?";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, email);
+            stmt.setString(2, senha);
+
+            ResultSet rs = stmt.executeQuery();
+
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Erro ao buscar usuário no banco de dados.", e);
+        }
+        return;
+    }
+
+
+}
