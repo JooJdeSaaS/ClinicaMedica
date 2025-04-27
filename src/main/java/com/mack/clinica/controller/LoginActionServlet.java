870587d3e3861fa199a7c99442cd58b7fe2ec6fe
fenrir-mack <10436455@mackenzista.com.br>
2025-04-27 05:32:10 -0300

    remover print para teste

--- src/main/java/com/mack/clinica/controller/LoginActionServlet.java
+++ src/main/java/com/mack/clinica/controller/LoginActionServlet.java
@@ -34,7 +34,6 @@ public class LoginActionServlet extends HttpServlet {
             HttpSession session = request.getSession();
             session.setAttribute("id", usuario.getId());
             session.setAttribute("nome", usuario.getNome());
-            System.out.println(session.getAttribute("nome"));
             session.setAttribute("tipo", usuario.getTipo());
             session.setAttribute("cpf", usuario.getCPF());
             session.setAttribute("celular", usuario.getCelular());
